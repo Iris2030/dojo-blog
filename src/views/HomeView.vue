4d400b211ed0dbdad47ed646bd3c14554a383377
Iris2030 <ibazhinova@gmail.com>
2023-01-06 14:21:06 +0200

    first commit

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,34 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div v-if="error">{{ error }}</div>
+    <div v-if="posts.length">
+      <PostList v-if="showPosts" :posts="posts"/>
+    </div>
+    <div v-else>...loading</div>
+<button @click="showPosts = !showPosts">Toggle posts</button>
+<button @click="posts.pop()">Deleta a posts</button>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import getPosts from '../composables/getPosts'
+import PostList from '../components/PostList.vue'
+import { ref } from 'vue'
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
-  }
+    PostList,
+  },
+
+  setup(){
+ 
+  const {posts, error, load} = getPosts()
+  const showPosts = ref(true)
+
+  load()
+
+  return {posts, showPosts, error }
+}
 }
 </script>
